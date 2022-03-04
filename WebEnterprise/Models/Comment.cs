2a043e74be6b0ab225f3b455b86939c0b76bd9df
Ngô Tuấn Anh <anhntgch190543@fpt.edu.vn>
2022-03-04 18:04:00 +0700

    abc

---
+++ WebEnterprise/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEnterprise.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int IdCommment { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public int Like { get; set; } = 0;
+
+        [Required(ErrorMessage = "Please enter Content")]
+        public string Content { get; set; }
+        //public int IdeaID { get; set; }
+        //public Idea Idea { get; set; }
+
+
+    }
+}
